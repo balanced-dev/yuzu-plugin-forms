7ae64f64dd084bd67ff32cd33b8e74a945863b57
philwhittaker <phil@hifi.agency>
2023-01-11 11:08:32 +0000

    change plugin folder addition precedence to before

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ module.exports = {
         return require.context('./_dev/_source/js/controllers', true, /\.js$/);
     },
     initForYuzuLoader: (config) => {
-        config.renderedPartialDirs.push('./node_modules/yuzu-plugin-forms/_dev/_templates/blocks');
-        config.renderedPartialDirs.push('./node_modules/yuzu-plugin-forms/_dev/_templates/_dataStructures');
-        config.registeredPartialsDirs.push('./node_modules/yuzu-plugin-forms/_dev/_templates/blocks');
-        config.dependantDirectories.push('./node_modules/yuzu-plugin-forms/_dev/_templates/blocks');
+        config.renderedPartialDirs.splice(0, 0, './node_modules/yuzu-plugin-forms/_dev/_templates/blocks');
+        config.renderedPartialDirs.splice(0, 0, './node_modules/yuzu-plugin-forms/_dev/_templates/_dataStructures');
+        config.registeredPartialsDirs.splice(0, 0, './node_modules/yuzu-plugin-forms/_dev/_templates/blocks');
+        config.dependantDirectories.splice(0, 0, './node_modules/yuzu-plugin-forms/_dev/_templates/blocks');
     },
     initForYuzuApi: (config) => {
-        config.api.files.templates.push('./node_modules/yuzu-plugin-forms/_dev/_templates');
+        config.api.files.templates.splice(0, 0, './node_modules/yuzu-plugin-forms/_dev/_templates');
     }
 };
